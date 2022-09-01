ffc5f87ef0853e86d734c594128c577841eb6665
Daniel Chinemerem <danielchinemerem302@gmail.com>
2022-09-01 16:39:37 +0100

    now able to display other users

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,29 +1,50 @@
-import { Box, Center, Flex, Grid, GridItem } from '@chakra-ui/react';
+import {
+  // Box,
+  // Center,
+  // Flex,
+  // Grid,
+  // GridItem,
+  SimpleGrid,
+} from '@chakra-ui/react';
 import { AgoraVideoPlayer } from 'agora-rtc-react';
 import { useEffect, useState } from 'react';
 const Video = ({ tracks, users }) => {
   const [gridSpacing, setGridSpacing] = useState(12);
 
   useEffect(() => {
-    setGridSpacing(Math.max(Math.floor(12 / (users.length + 1)), 4));
+    // setGridSpacing(Math.max(Math.floor(12 / (users.length + 1)), 4));
+    setGridSpacing((users.length + 1) % 4);
   }, [users, tracks]);
 
   return (
-    <Grid>
-      <GridItem colSpan={gridSpacing} h="100%" w="70%">
-        <AgoraVideoPlayer videoTrack={tracks[1]} />
-      </GridItem>
+    <SimpleGrid columns={gridSpacing} spacing={10} height="100vh">
+      <AgoraVideoPlayer videoTrack={tracks[1]} />
       {users.length > 0 &&
         users.map(user => {
           if (user.videoTrack) {
             return (
-              <GridItem colSpan={gridSpacing} height="100%" width="500px">
-                <AgoraVideoPlayer videoTrack={user.videoTrack} key={user.uid} />
-              </GridItem>
+              <AgoraVideoPlayer videoTrack={user.videoTrack} key={user.uid} />
             );
           } else return null;
         })}
-    </Grid>
+    </SimpleGrid>
+
+    // <Grid>
+    //   <GridItem colSpan={gridSpacing} h="100%" w="70%">
+    //     <AgoraVideoPlayer videoTrack={tracks[1]} />
+    //   </GridItem>
+    //   {users.length > 0 &&
+    //     users.map(user => {
+    //       if (user.videoTrack) {
+    //         return (
+    //           <GridItem colSpan={gridSpacing} height="100%" width="500px">
+    //             <AgoraVideoPlayer videoTrack={user.videoTrack} key={user.uid} />
+    //           </GridItem>
+    //         );
+    //       } else return null;
+    //     })}
+    // </Grid>
+
     // <Box h="100%">
     //   <Center>
     //     <Grid h="100%" w="70%">
